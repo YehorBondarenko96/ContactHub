4d005f915d756991f5a596bf9d5e49beff30ab7a
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-02-06 13:41:13 +0200

    0.1.0-06.02.24

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>
   {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter basename='/YehorBondarenko96/ContactHub'>
+    <BrowserRouter basename='/ContactHub'>
     <App />
     </BrowserRouter>
   {/* </PersistGate> */}
